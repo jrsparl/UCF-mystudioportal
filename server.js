d8200bc89e38e80551e52c44f00b5a92606f8c02
Napo <Michaeljnapolitano@gmail.com>
2020-11-29 17:19:30 -0500

    added session expires after 1 hour

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
     secret: process.env.DB_SECRET,
-    cookie: {},
+    cookie: {
+        expires: 60000000,
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
