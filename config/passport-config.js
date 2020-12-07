892e06818d0de333c1a640481e72b7cf2fb122dd
Napo <Michaeljnapolitano@gmail.com>
2020-12-06 20:24:05 -0500

    still messing with it

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,29 +1,41 @@
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
+const { User, Teacher, Student } = require("../models");
+var passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy;
 
-function initialize(passport, getUserByEmail, getUserById) {
-  const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email)
-    if (user == null) {
-      return done(null, false, { message: 'No user with that email' })
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "passwd",
+    },
+    function (username, password, done) {
+      User.findOne(
+        {
+          username: username,
+          include: [
+            {
+              model: Student,
+            },
+            {
+              model: Teacher,
+            },
+          ],
+        },
+        function (err, user) {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false, { message: "Incorrect username." });
+          }
+          if (!user.validPassword(password)) {
+            return done(null, false, { message: "Incorrect password." });
+          }
+          return done(null, user);
+        }
+      );
     }
+  )
+);
 
-    try {
-      if (await bcrypt.compare(password, user.password)) {
-        return done(null, user)
-      } else {
-        return done(null, false, { message: 'Password incorrect' })
-      }
-    } catch (e) {
-      return done(e)
-    }
-  }
-
-  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-  passport.serializeUser((user, done) => done(null, user.id))
-  passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id))
-  })
-}
-
-module.exports = initialize
+module.exports = passport;
