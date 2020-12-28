f63aa90bb24152e6cbddda1be611aebe3952262b
Napo <Michaeljnapolitano@gmail.com>
2020-12-28 01:32:21 -0500

    cloudinary upload widget and post working with everything inside addsong.handlebars file

--- public/javascript/addsong.js
+++ public/javascript/addsong.js
@@ -1,33 +1,33 @@
-async function SongFormHandler(event) {
-  event.preventDefault();
+// async function SongFormHandler(event) {
+//   event.preventDefault();
 
-  const song_name = document.querySelector("#song-name").value.trim();
-  const album_name = document.querySelector("#song-album").value.trim();
-  const song_writer = document.querySelector("#song-artist").value.trim();
-  const company_id = document.getElementById("company-id").textContent;
-  const path = fileName;
+//   const song_name = document.querySelector("#song-name").value.trim();
+//   const album_name = document.querySelector("#song-album").value.trim();
+//   const song_writer = document.querySelector("#song-artist").value.trim();
+//   const company_id = document.getElementById("company-id").textContent;
+//   const path = result.info.path;
 
-  const response = await fetch("/api/teachers/song", {
-    method: "post",
-    body: JSON.stringify({
-      song_name,
-      album_name,
-      song_writer,
-      company_id,
-      path,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
+//   const response = await fetch("/api/teachers/song", {
+//     method: "post",
+//     body: JSON.stringify({
+//       song_name,
+//       album_name,
+//       song_writer,
+//       company_id,
+//       path,
+//     }),
+//     headers: { "Content-Type": "application/json" },
+//   });
 
-  if (response.ok) {
-    console.log("song added");
-    document.location.replace("/teacherhome/addsong");
-  } else {
-    console.log("oh no");
-    alert(response.statusText);
-  }
-}
+//   if (response.ok) {
+//     console.log("song added");
+//     document.location.replace("/teacherhome/addsong");
+//   } else {
+//     console.log("oh no");
+//     alert(response.statusText);
+//   }
+// }
 
-document
-  .querySelector(".audio-upload")
-  .addEventListener("submit", SongFormHandler);
+// document
+//   .querySelector(".audio-upload")
+//   .addEventListener("submit", SongFormHandler);
